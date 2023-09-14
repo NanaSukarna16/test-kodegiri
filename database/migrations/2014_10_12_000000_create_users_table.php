f8a6b2a2af7832b6f39adfd96db4d76dc58f69bd
NanaSukarna16 <nana16.sukarna6@gmail.com>
2023-09-14 16:27:24 +0700

    feat: membuat dan menyesuaikan tabel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nama_lengkap');
+            $table->string('nomor_hp');
+            $table->string('company')->nullable();
+            $table->string('divisi')->nullable();
+            $table->string('foto_profil')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
